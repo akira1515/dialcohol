0107f5860abf17fe67e3f59d7b61539728c1685d
akira1515 <akira.kobayashi1515@gmail.com>
2023-12-10 17:09:20 +0900

    ローディング画面を表示させる

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,8 @@ export default function Home() {
     questions.reduce((prev, curr) => ({ ...prev, [curr.id]: null }), {})
   );
   const [showErrorMessage, setShowErrorMessage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
 
   const handleBoxClick = (questionId: number, value: number) => {
     setSelectedBox(prev => ({ ...prev, [questionId]: value }));
@@ -38,14 +40,23 @@ export default function Home() {
       return { type: '未定義のパーソナリティ', description: '' };
     }
   };
-  
 
+  const LoadingScreen = () => (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <img src="/beer-loding.gif"/>
+    </div>
+  );  
+  
   const handleSubmit = () => {
     const isAllAnswered = !Object.values(selectedBox).some(score => score === null);
   
     if (isAllAnswered) {
+      setIsLoading(true); // ロード開始
       const { type, description } = calculatePersonality();
+      setTimeout(() => {
+        setIsLoading(false); // ロード終了
       router.push(`/results?personalityType=${encodeURIComponent(type)}&description=${encodeURIComponent(description)}`);
+      }, 3000);
     } else {
       setShowErrorMessage(true);
     }
@@ -54,49 +65,55 @@ export default function Home() {
   return (
     <Box sx={{ py: 8, display: "flex", flexDirection: "column", alignItems: "center" }}>
       <Header />
-      {questions.map((question:Question, index: number) => (
-        <Box key={question.id} sx={{mt:10,  mx:"auto", width: '100%', maxWidth:"800px",}}>
-        <Typography sx={{
-            fontSize: "2rem", 
-            fontWeight:"bold", 
-            '@media (max-width: 500px)': {
-              fontSize: "1.2rem"
-            }
-          }}>Q{index + 1}, {question.textQuestion}</Typography>
-        <Box
-          sx={{
-            mt:10,
-            mx:"auto",
-            display: 'grid',
-            gridTemplateColumns: 'repeat(2, 1fr)',
-            gridTemplateRows: 'repeat(2, 1fr)',
-            gap: '10px',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-            maxWidth:"800px",
-          }}
-        >
-          {question.textSelect.map((text, index) => (
-              <CircleBox
-                key={index}
-                onClick={() => handleBoxClick(question.id, index)}
-                selected={selectedBox[question.id] === index}
-                text={text}
-              />
-            ))}
-        </Box>
-        </Box>
-      ))}
-      {!showErrorMessage && (
-        <Button variant="contained" sx={{ fontSize: '1.4rem', mt: 4, width: "30%", '@media (max-width: 500px)': { fontSize: "1.2rem" }}} onClick={handleSubmit}>
-          診断する
-        </Button>
-      )}
-      {showErrorMessage && (
-        <Typography sx={{ color: 'red', mt: 2 }}>
-          全ての問いに回答してください
-        </Typography>
+      {isLoading ? (
+      <LoadingScreen />
+    ) : (
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        {questions.map((question:Question, index: number) => (
+          <Box key={question.id} sx={{mt:10,  mx:"auto", width: '100%', maxWidth:"800px",}}>
+          <Typography sx={{
+              fontSize: "2rem", 
+              fontWeight:"bold", 
+              '@media (max-width: 500px)': {
+                fontSize: "1.2rem"
+              }
+            }}>Q{index + 1}, {question.textQuestion}</Typography>
+          <Box
+            sx={{
+              mt:10,
+              mx:"auto",
+              display: 'grid',
+              gridTemplateColumns: 'repeat(2, 1fr)',
+              gridTemplateRows: 'repeat(2, 1fr)',
+              gap: '10px',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%',
+              maxWidth:"800px",
+            }}
+          >
+            {question.textSelect.map((text, index) => (
+                <CircleBox
+                  key={index}
+                  onClick={() => handleBoxClick(question.id, index)}
+                  selected={selectedBox[question.id] === index}
+                  text={text}
+                />
+              ))}
+          </Box>
+          </Box>
+        ))}
+        {!showErrorMessage && (
+          <Button variant="contained" sx={{ fontSize: '1.4rem', mt: 4, width: "30%", '@media (max-width: 500px)': { fontSize: "1.2rem" }}} onClick={handleSubmit}>
+            診断する
+          </Button>
+        )}
+        {showErrorMessage && (
+          <Typography sx={{ color: 'red', mt: 2 }}>
+            全ての問いに回答してください
+          </Typography>
+        )}
+      </div>
       )}
     </Box>
   );
