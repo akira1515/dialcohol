43ef6aaa07f2becab3ba4505a82b53b49bbc5d9d
akira1515 <akira.kobayashi1515@gmail.com>
2023-12-10 16:21:47 +0900

    「飲みの場ではこんな人です」という説明文を追加

--- pages/results.tsx
+++ pages/results.tsx
@@ -6,11 +6,17 @@ import { Box, Button, Typography } from '@mui/material';
 const Results = () => {
   const router = useRouter();
   const [personalityType, setPersonalityType] = useState('');
+  const [description, setDescription] = useState('');
 
   useEffect(() => {
     const type = router.query.personalityType;
-    if (type) {
-      setPersonalityType(Array.isArray(type) ? type[0] : decodeURIComponent(type));
+    const typeDescription = router.query.description;
+  
+    if (type && typeDescription) {
+      const decodedType = Array.isArray(type) ? type[0] : decodeURIComponent(type);
+      const decodedDescription = Array.isArray(typeDescription) ? typeDescription[0] : decodeURIComponent(typeDescription);
+      setPersonalityType(decodedType);
+      setDescription(decodedDescription);
     }
   }, [router.query]);
   
@@ -27,6 +33,12 @@ const Results = () => {
         {personalityType && (
           <span style={{ fontSize: "2rem", fontWeight: "bold" }}>{personalityType}</span>
         )}
+        <br /><br />
+        〜飲みの場ではこんな人です〜<br />
+        {description && (
+          <span style={{ fontSize: "1rem", margin: "5px 30px", width: "70%" }}>{description}</span>
+        )}
+
       </Typography>
       <Button variant="contained" sx={{ fontSize: '1.4rem', mt: 4, width: "30%", '@media (max-width: 500px)': {fontSize: "1.2rem"} }} onClick={handleReset}>
         やり直す
