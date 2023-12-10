239cf413d9fabb296c096b0b8da88e01200fbfcf
akira1515 <akira.kobayashi1515@gmail.com>
2023-12-10 15:02:23 +0900

    「やり直す」ボタンを追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,12 +37,23 @@ export default function Home() {
 
   const [message, setMessage] = useState<string>("");
   const [personalityType, setPersonalityType] = useState<string>("");
+  const [isDiagnosed, setIsDiagnosed] = useState(false);
+
 
   const handleSubmit = () => {
     const personality = calculatePersonality();
     setPersonalityType(personality);
     setMessage(`あなたの性格タイプは`);
+    setIsDiagnosed(true);
   }
+  const handleReset = () => {
+    setSelectedBox(questions.reduce((prev, curr) => ({ ...prev, [curr.id]: null }), {}));
+    setMessage("");
+    setPersonalityType("");
+    setIsDiagnosed(false);
+    window.scrollTo(0, 0);
+  };
+
 
   return (
     <Box sx={{py:8, display:"flex", flexDirection:"column", alignItems:"center"}}>
@@ -81,6 +92,7 @@ export default function Home() {
         </Box>
         </Box>
       ))}
+      {!isDiagnosed && (
       <Button variant="contained" sx={{
               fontSize: '2rem', 
               mt:4, 
@@ -89,6 +101,17 @@ export default function Home() {
               fontSize: "1.2rem"
               }
             }} onClick={handleSubmit}>診断する</Button>
+      )}
+      {isDiagnosed && (
+      <Button variant="contained" sx={{ 
+        fontSize: '2rem', 
+        mt: 2, 
+        width: "30%", 
+        '@media (max-width: 500px)': {
+          fontSize: "1.2rem"
+        }
+      }} onClick={handleReset}>やり直す</Button>
+    )}
       <Typography sx={{
               mt:4, 
               fontSize:"2rem", 
