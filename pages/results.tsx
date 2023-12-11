c1c4917598cdf80104c22724de2e7e4ea7845891
akira1515 <akira.kobayashi1515@gmail.com>
2023-12-11 12:27:06 +0900

    xに投稿してもらうための文章

--- pages/results.tsx
+++ pages/results.tsx
@@ -41,19 +41,22 @@ const Results = () => {
           <span style={{ fontSize: "2rem", fontWeight: "bold" }}>{personalityType}</span>
         )}
         <span style={{ display: "block" }}>のような人！</span>
+        <br />
+        {description && (
+          <img src={image} style={{ width: "280px"}}/>
+        )}
         <br /><br />
         〜飲みの場ではこんな人です〜<br />
         {description && (
           <span style={{ fontSize: "1rem", margin: "5px 30px", width: "70%" }}>{description}</span>
         )}
-        <br />
-        {description && (
-          <img src={image} style={{ width: "70%"}}/>
-        )}
 
       </Typography>
+      <Typography sx={{ mt: 4, fontSize: "0.9rem", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        名刺をXで投稿してくれた人の中から<br />抽選でクーポンをプレゼント！
+      </Typography>
       <Button
-        variant="contained" sx={{ fontSize: '1.4rem', mt: 4, width: "30%", '@media (max-width: 500px)': {fontSize: "1.2rem"} }}
+        variant="contained" sx={{ fontSize: '1.4rem', mt: 2, width: "30%", '@media (max-width: 500px)': {fontSize: "1.2rem"} }}
         onClick={() => window.open(twitterShareUrl, '_blank')}
       >
         𝕏で共有
